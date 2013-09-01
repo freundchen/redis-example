653d45f00f620bee0eb7d7e31c3933bd1f6f79be
Enno Brehm <brehm@amazon.com>
2013-09-01 12:57:27 +0200

    Initial import.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+RedisExample::Application.config.secret_key_base = 'e23c946637fdca0edd0bd6ada3b7e8dcf7c2d401fc945e45e3f27367e9bdc9c4382414fd7e7d9a9bbec03f98ff6554059bc1e8132b2c136770230a63138cad67'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+RedisExample::Application.config.session_store :cookie_store, key: '_redis-example_session'
